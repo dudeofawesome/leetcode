2722244bcca806181327c93ad48819f83260a42a
Louis Orleans <louis@orleans.io>
2021-12-23 01:09:14 -0700

    ♻️ move listnode helpers tests to their own file

--- 143-reorder-list/solution.test.ts
+++ 143-reorder-list/solution.test.ts
@@ -1,32 +1,10 @@
 import { assertEquals } from 'https://deno.land/std@0.118.0/testing/asserts.ts';
-import { ArrayToList, ListToArray, ListNode } from '../helpers/list-node.ts';
+import { ArrayToList, ListToArray } from '../helpers/list-node.ts';
 import { _export as solution } from './solution.ts';
 
 // sleep long enough for the Deno debugger to catch up
 // https://github.com/denoland/vscode_deno/issues/557#issuecomment-986292261
 await new Promise(resolve => setTimeout(resolve, 2000));
-
-Deno.test('ArrayToList', () => {
-  let actual: ListNode | null | undefined = ArrayToList([1, 2, 3, 4]);
-  let expected: ListNode | null | undefined = new ListNode(
-    1,
-    new ListNode(2, new ListNode(3, new ListNode(4))),
-  );
-  while (actual || expected) {
-    assertEquals(actual?.val, expected?.val);
-    actual = actual?.next;
-    expected = expected?.next;
-  }
-});
-
-Deno.test('ListToArray', () => {
-  let actual = ListToArray(
-    new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))),
-  );
-  let expected = [1, 2, 3, 4];
-
-  assertEquals(actual, expected);
-});
 
 Deno.test('null', () => {
   assertEquals(solution(), null);
