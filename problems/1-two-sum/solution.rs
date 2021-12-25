e6e2314acd3763faa3ae9f75b0889e0702f0123c
Louis Orleans <louis@orleans.io>
2021-12-24 23:24:59 -0700

    🚧✨ add Rust support

---
+++ problems/1-two-sum/solution.rs
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1 lang=rust
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+impl Solution {
+  pub fn count_vowel_strings(n: i32) -> i32 {
+    return 0;
+  }
+}
+// @lc code=end
+
+#[cfg(test)]
+mod tests {
+  // Note this useful idiom: importing names from outer (for mod tests) scope.
+  use super::*;
+
+  #[test]
+  fn test_add() {
+    assert_eq!(add(1, 2), 3);
+  }
+
+  #[test]
+  fn test_bad_add() {
+    // This assert would fire and test will fail.
+    // Please note, that private functions can be tested too!
+    assert_eq!(bad_add(1, 2), 3);
+  }
+}
