85aa0b66aaf3ebeb5af63adfad775a6719bef478
Louis Orleans <louis@orleans.io>
2021-12-26 20:00:45 -0700

    ✨ solve problem 973
    how on earth was this a "medium"?

---
+++ problems/973-k-closest-points-to-origin/solution.test.ts
@@ -0,0 +1,36 @@
+import { assertEquals } from 'https://deno.land/std@0.118.0/testing/asserts.ts';
+import { _export as solution } from './solution.ts';
+
+// sleep long enough for the Deno debugger to catch up
+// https://github.com/denoland/vscode_deno/issues/557#issuecomment-986292261
+await new Promise(resolve => setTimeout(resolve, 2000));
+
+Deno.test('[[1,3],[-2,2]], 1', () =>
+  assertEquals(
+    solution(
+      [
+        [1, 3],
+        [-2, 2],
+      ],
+      1,
+    ),
+    [[-2, 2]],
+  ),
+);
+
+Deno.test('[[3,3],[5,-1],[-2,4]], 2', () =>
+  assertEquals(
+    solution(
+      [
+        [3, 3],
+        [5, -1],
+        [-2, 4],
+      ],
+      2,
+    ),
+    [
+      [3, 3],
+      [-2, 4],
+    ],
+  ),
+);
